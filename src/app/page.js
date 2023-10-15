5fa8e99cc9cb8c96ef8c4c5ffadc8c5678c0cbce
linqingli <lancellq@gmail.com>
2023-10-15 15:26:16 -0700

    step : event bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 import Landing from "@/components/home/Landing";
-
+import Event from "@/components/events/Event";
 const home = () => {
   return (
+    
     <div>
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <Event />
+          <br/>
+          <Event month='JAN' day= '31' time='25AM' Event ='Is there 25AM'  description= 'yes there is, also exist Jan32' color="bg-blue-500" />
+        </div>
+
       <Landing />
     </div>
   );
