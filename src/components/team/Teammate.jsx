a86d107966430a68e2399c4006d7ad733d9ff6e9
Ricky Wang <rwang187@ucr.edu>
2023-10-16 10:18:09 -0700

    my first commit didn't seem to go through

--- src/components/team/Teammate.jsx
+++ src/components/team/Teammate.jsx
@@ -1,37 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
-import './teamAlignment.css';
-
 const Teammate = () => {
-  return (
-    <div class="bg-gradient-to-r from-blue-300 to-black ...">
-    <div className="container">
-      <div className="image">
-        <Image
-          src="team/graves.png" // Path to your image inside the 'public' directory
-          alt="Graves Image"
-          width={500} // Desired width of the image
-          height={300} // Desired height of the image
-        />
-      </div>
-
-      <div className="text">
-        <div className="gradient-text">
-        <div className="custom-font">
-        <p>Matthew "GetTurtled"</p> 
-        <p>Nguyen</p>
-        </div>
-        </div>
-        <div className="info">
-        <p>YEAR: 3</p> 
-        <p>POSITION: JG</p>
-        </div>
-      </div>
-
-    
-    </div>
-    </div>
-  );
+  return <div>Teammate</div>;
 };
 
 export default Teammate;
