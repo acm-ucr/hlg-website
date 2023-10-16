bbaed71016cdc232e24c55f46c094bb8583cd65e
Ricky Wang <rwang187@ucr.edu>
2023-10-16 10:09:01 -0700

    removed the .css file and remembered to grab a screenshot

--- src/components/events/Slides.jsx
+++ src/components/events/Slides.jsx
@@ -1,35 +1,30 @@
-import React from "react";
 import Image from "next/image";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import "./slidesAlignment.css";
-
 
 const Slides = () => {
   return (
     <div className="Slides">
-      <div className="container">
-        <div className="leftArrow">
+      <div className="flex justify-center items-center">
+        <div className="my-55 mr-40">
           <AiOutlineArrowLeft
             size={50}
             className="text-asme-blue-600 cursor-pointer text-xl"
           />
         </div>
 
-
-        <div className="text">
-          <p>Event Name</p>
-          <div className="image">
-            <Image
-              src="registration-line.png" // Path to your image inside the 'public' directory
-              alt="registration line Image"
-              width={700} // Desired width of the image
-              height={300} // Desired height of the image
-            />
+        <div className="center section">
+          <div className="font-playfair text-lg pt-4">
+            <p>Event Name</p>
           </div>
+          <Image
+            src="registration-line.png" // Path to your image inside the 'public' directory
+            alt="registration line Image"
+            width={600} // Desired width of the image
+            height={200} // Desired height of the image
+          />
         </div>
 
-
-        <div className="rightArrow">
+        <div className="my-55 ml-40">
           <AiOutlineArrowRight
             size={50}
             className="text-asme-blue-600 cursor-pointer text-xl"
@@ -40,7 +35,4 @@ const Slides = () => {
   );
 };
 
-
 export default Slides;
-
-
