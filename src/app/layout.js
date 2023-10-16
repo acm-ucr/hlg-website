5476e208d6336f4704d009382d7faf098fe53187
Ricky Wang <rwang187@ucr.edu>
2023-10-16 15:52:35 -0700

    issue 8

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Montserrat } from "next/font/google";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import Teammate from "../components/team/Teammate";
+import Slides from "../components/events/Slides";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -16,14 +18,20 @@ export const metadata = {
   description: "Highlander Gaming at UCR",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        <Navigation />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={montserrat.className}>
+//         <Navigation />
+//         {children}
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
+
+function Test() {
+  return <Teammate />;
 }
+
+export default Test;
--- src/components/team/Teammate.jsx
+++ src/components/team/Teammate.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import Image from 'next/image';
-import './teamAlignment.css';
+import React from "react";
+import Image from "next/image";
+import "./teamAlignment.css";
 
 const Teammate = () => {
   return (
-    <div class="bg-gradient-to-r from-blue-300 to-black ...">
-    <div className="container">
-      <div className="image">
-        <Image
-          src="team/graves.png" // Path to your image inside the 'public' directory
-          alt="Graves Image"
-          width={500} // Desired width of the image
-          height={300} // Desired height of the image
-        />
-      </div>
+    <div className="bg-gradient-to-r from-hlg-blue-300 to-hlg-black ...">
+      <div className="flex">
+        <div className="mr-50">
+          <Image
+            src="team/graves.png" // Path to your image inside the 'public' directory
+            alt="Graves Image"
+            width={500} // Desired width of the image
+            height={300} // Desired height of the image
+          />
+        </div>
 
-      <div className="text">
-        <div className="gradient-text">
-        <div className="custom-font">
-        <p>Matthew "GetTurtled"</p> 
-        <p>Nguyen</p>
-        </div>
-        </div>
-        <div className="info">
-        <p>YEAR: 3</p> 
-        <p>POSITION: JG</p>
+        <div className="mt-10 ml-10">
+          <div className="bg-gradient-to-r from-hlg-blue-100 to-hlg-blue-200 bg-clip-text text-transparent">
+            <div className="font-bold leading-tight text-5xl -mb-3">
+              <p>Matthew &quot;GetTurtled&quot;</p>
+              <p>Nguyen</p>
+            </div>
+          </div>
+          <div className="mb-7 text-hlg-white p-3 leading-3">
+            <p>YEAR: 3</p>
+            <p>POSITION: JG</p>
+          </div>
         </div>
       </div>
-
-    
-    </div>
     </div>
   );
 };
