ff95493a0862356f1e8416d8b39fe42f876b1d92
Ricky Wang <rwang187@ucr.edu>
2023-10-18 16:22:56 -0700

    approved version in meeting

--- src/components/team/Teammate.jsx
+++ src/components/team/Teammate.jsx
@@ -5,15 +5,15 @@ import Graves from "public/team/graves.png";
 const Teammate = () => {
   return (
     <div className="bg-gradient-to-r from-hlg-blue-300 to-hlg-black">
-      <div className="flex">
+      <div className="flex ">
         <Image
           src={Graves} // Path to your image inside the 'public' directory
           alt="Graves Image"
         />
 
-        <div className="mt-10 ml-10">
+        <div className="mt-10 pl-10">
           <div className="bg-gradient-to-r from-hlg-blue-100 to-hlg-blue-200 bg-clip-text text-transparent">
-            <div className="font-extrabold leading-tight text-5xl -mb-3 m-0 p-0">
+            <div className="font-bold leading-tight text-5xl -mb-3 m-0 p-0">
               <p>Matthew &quot;GetTurtled&quot;</p>
               <p>Nguyen</p>
             </div>
