41ac7c289fc0c2fa2d0c8433ab676c4daee30e0e
javi <javi@Javis-MacBook-Pro-3.local>
2023-10-18 11:22:20 -0700

    Add 404 Page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,30 @@
 const error = () => {
-  return <div>error</div>;
+  return (
+    <div className=" flex flex-col my-20 items-center">
+      <p className="text-hlg-orange font-bold font-serif text-center text-4xl">
+        {" "}
+        Whoops!{" "}
+      </p>
+
+      <p className="text-hlg-blue-300 text-center font-bold font-serif text-8xl ">
+        {" "}
+        404{" "}
+      </p>
+
+      <p className=" text-hlg-orange font-bold font-mono text-center text-3xl">
+        {" "}
+        Page Not Found{" "}
+      </p>
+
+      <a
+        href="/"
+        className="text-hlg-blue-300 font-serif font-semibold text-center text-2xl"
+      >
+        {" "}
+        Back To Home Page{" "}
+      </a>
+    </div>
+  );
 };
 
 export default error;
