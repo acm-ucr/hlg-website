3ed399bf90e1303efc759f90f81f95c1a5de5b44
Lanceart <lancellq@gmail.com>
2023-10-18 16:38:56 -0700

    step: remain font-bold bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Landing from "@/components/home/Landing";
+import Event from "@/components/events/Event";
 const home = () => {
   return (
     <div>
--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -1,5 +1,7 @@
 "use client";
+import Link from "next/link";
 import React from "react";
+import { FiExternalLink } from "react-icons/fi";
 const Event = ({
   month = "FEB",
   day = "29",
@@ -10,23 +12,25 @@ const Event = ({
   color = "bg-hlg-orange",
 }) => {
   return (
-    <div className="flex rounded-lg overflow-hidden shadow-md max-w-lg">
-      <div className={`${color} p-4 text-white text-center w-1/3`}>
-        <div className="text-2xl font-bold">{month}</div>
-        <div className="text-4xl ">{day}</div>
-        <div className="text-xl">{time}</div>
+    <div className="flex rounded-lg overflow-hidden  max-w-2xl">
+      <div className={`${color} p-4 text-hlg-white text-center w-1/4`}>
+        <div className="text-4xl font-bold">{month}</div>
+        <div className="text-4xl font-black ">{day}</div>
+        <div className="text-xl font-extrabold">{time}</div>
       </div>
-      <div className="flex-1 pl-4">
-        <p className="w-full text-xl font-bold mb-1 focus:outline-none resize-none font-bold">
-          {title}
+
+      <div className="flex-1 pl-4 bg-hlg-gray">
+        <p className="text-xl text-hlg-black font-extrabold mb-1">{title}</p>
+        <div className="flex items-center text-hlg-black">
+          <p className="flex m-0 p-0 font-bold">zoom: link</p>
+
+          <Link className="text-hlg-black" href={`${link}`}>
+            <FiExternalLink />
+          </Link>
+        </div>
+        <p className="w-full focus:outline-none resize-none font-bold">
+          {description}
         </p>
-        <a
-          href={`${link}`}
-          className="w-full text-blue-500 mb-2 focus:outline-none resize-none"
-        >
-          zoom: link
-        </a>
-        <p className="w-full focus:outline-none resize-none">{description}</p>
       </div>
     </div>
   );
