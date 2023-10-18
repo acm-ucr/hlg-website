daffbc765b48301621c01af3fa924cc32dd2ed62
Tuan Do <trinhphdo@gmail.com>
2023-10-17 19:10:46 -0700

    added navbar logo

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
 const Icon = () => {
-  return <div>Icon</div>;
+  return (
+    <div className="container">
+      <div className="image">
+        <Image src="navbar-logo.svg" alt="Navbar-Logo" width={57} height={64} />
+      </div>
+    </div>
+  );
 };
 
 export default Icon;
