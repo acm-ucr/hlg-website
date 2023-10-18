670baed88dfbbf02c0f22669a2f19bd62dff424f
javi <javi@Javis-MacBook-Pro-3.local>
2023-10-18 13:22:55 -0700

    Resolve font, underline, and border issues

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,24 +1,16 @@
 const error = () => {
   return (
     <div className=" flex flex-col my-20 items-center">
-      <p className="text-hlg-orange font-bold font-serif text-center text-4xl">
-        {" "}
-        Whoops!{" "}
-      </p>
+      <p className="text-hlg-orange text-center text-4xl"> Whoops! </p>
 
-      <p className="text-hlg-blue-300 text-center font-bold font-serif text-8xl ">
-        {" "}
-        404{" "}
-      </p>
+      <p className="text-hlg-blue-300 text-center text-8xl "> 404 </p>
 
-      <p className=" text-hlg-orange font-bold font-mono text-center text-3xl">
-        {" "}
-        Page Not Found{" "}
-      </p>
+      <p className=" text-hlg-orange text-center text-3xl"> Page Not Found </p>
 
       <a
         href="/"
-        className="text-hlg-blue-300 font-serif font-semibold text-center text-2xl"
+        className="text-hlg-blue-300 text-center text-2xl border border-hlg-gray rounded p-2"
+        style={{ textDecoration: "none" }}
       >
         {" "}
         Back To Home Page{" "}
