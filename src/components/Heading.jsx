3765a30d2fef712bf1db654c4e66d6f27e1cd6b6
John Zhou <zhou9865@gmail.com>
2023-10-18 17:22:43 -0700

    ignore this

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import photo from "public/colored-graph.svg";
-const Heading = () => {
-  return <Image src={photo} />;
+const Heading = ({ text }) => {
+  return (
+    <div className="flex gap-10">
+      <Image src={photo} />
+      <div className="flex-col">
+        <h2 className="top-2 left-4"> {text}</h2>
+        <div className="flex">
+          <div className="bg-hlg-orange p-3"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Heading;
