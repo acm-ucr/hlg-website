e8b2eede322a9378b4ce4872316f28116798e561
theo <theobfernandez@gmail.com>
2023-10-20 11:33:26 -0700

    add team data

--- src/data/team.js
+++ src/data/team.js
@@ -1,12 +1,52 @@
 import Graves from "public/team/graves.png";
+import Mordekaiser from "public/team/mordekaiser.png";
+import Gragas from "public/team/gragas.png";
+import Aphelios from "public/team/aphelios.png";
+import Rell from "public/team/rell.png";
 export const team = [
-  {
-    name: 'Matthew "GetTurtled" Nguyen',
-    image: Graves,
-    year: "3",
-    position: "JG",
-    champion: "Graves",
-    color: "hlg-blue-100",
-    colorGradient: "hlg-blue-200",
-  },
+	{
+		name: 'Brogan "dont play top" McCoy',
+		image: Mordekaiser,
+		year: "2",
+		position: "Top",
+		champion: "Mordekaiser",
+		color: "hlg-orange",
+		colorGradient: "hlg-yellow",
+	},
+	{
+		name: 'Matthew "GetTurtled" Nguyen',
+		image: Graves,
+		year: "3",
+		position: "JG",
+		champion: "Graves",
+		color: "hlg-blue-100",
+		colorGradient: "hlg-blue-200",
+	},
+	{
+		name: 'Jake "LDCs" Han',
+		image: Gragas,
+		year: "4",
+		position: "Mid",
+		champion: "Gragas",
+		color: "hlg-orange",
+		colorGradient: "hlg-yellow",
+	},
+	{
+		name: 'Andre "GALAMODE" Amante',
+		image: Aphelios,
+		year: "4",
+		position: "Bot/ADC",
+		champion: "Aphelios",
+		color: "hlg-pink",
+		colorGradient: "hlg-purple",
+	},
+	{
+		name: 'Chrisian "SlyLego" Tran',
+		image: Rell,
+		year: "4",
+		position: "Supp",
+		champion: "Rell",
+		color: "hlg-green",
+		colorGradient: "hlg-teal",
+	},
 ];
