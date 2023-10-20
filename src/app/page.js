853300e935e51a43329dffb6852c7aed9d30cdd4
Lanceart <lancellq@gmail.com>
2023-10-20 11:35:17 -0700

    step: finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 import Landing from "@/components/home/Landing";
-import Event from "@/components/events/Event";
 const home = () => {
   return (
     <div>
-      <Event />
       <Landing />
     </div>
   );
