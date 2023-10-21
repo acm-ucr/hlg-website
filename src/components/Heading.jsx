651815c4e6d29cb080deb688ef49fa239ee327a8
John Zhou <zhou9865@gmail.com>
2023-10-20 20:44:08 -0700

    deleted extra comment

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,16 +2,6 @@ import Image from "next/image";
 import photo from "public/colored-graph.svg";
 const Heading = ({ text }) => {
   return (
-    // <div className="flex gap-10">
-    //   <Image src={photo} />
-    //   <div className="flex-col">
-    //     <h1 className="font-weight:500 top-2 left-4"> {text}</h1>
-    //     <div className="flex">
-    //       <div className="bg-hlg-orange p-1"></div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="flex flex-col">
       <div className="flex flex-row">
         <div className="" style={{ paddingRight: "10px" }}>
