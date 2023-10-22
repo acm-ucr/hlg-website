fdc3a78b0dbe9509aa69883c55f60eb5c4a74856
Tuan Do <trinhphdo@gmail.com>
2023-10-21 18:05:08 -0700

    make icon object and add text

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,15 @@
-import React from "react";
 import Image from "next/image";
+import objectName from "../../public/navbar-logo.svg";
 
 const Icon = () => {
   return (
-    <div className="container">
-      <div className="image">
-        <Image src="navbar-logo.svg" alt="Navbar-Logo" width={57} height={64} />
+    <div className="flex">
+      <Image src={objectName} alt="Navbar-Logo" />
+      <div
+        className="font-extrabold text-white text-4xl -mb-4"
+        style={{ fontWeight: 2000, marginTop: "10px", marginLeft: "10px" }}
+      >
+        <div> HLG</div>
       </div>
     </div>
   );
