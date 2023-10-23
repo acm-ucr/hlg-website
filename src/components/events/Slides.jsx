45f1590ce5f6a84341cb4b88cf548209131db21c
Ricky Wang <rwang187@ucr.edu>
2023-10-23 10:58:33 -0700

    adjustments to format of slides

--- src/components/events/Slides.jsx
+++ src/components/events/Slides.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import eventPicture from "public/registration-line.png";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Slides = () => {
@@ -6,29 +7,21 @@ const Slides = () => {
     <div className="Slides">
       <div className="flex justify-center items-center">
         <div className="my-55 mr-40">
-          <AiOutlineArrowLeft
-            size={50}
-            className="text-asme-blue-600 cursor-pointer text-xl"
-          />
+          <AiOutlineArrowLeft size={50} className="cursor-pointer text-xl" />
         </div>
 
         <div className="center section">
-          <div className="font-playfair text-lg pt-4">
+          <div className="text-lg pt-4">
             <p>Event Name</p>
           </div>
           <Image
-            src="registration-line.png" // Path to your image inside the 'public' directory
+            src={eventPicture} // Path to your image inside the 'public' directory
             alt="registration line Image"
-            width={600} // Desired width of the image
-            height={200} // Desired height of the image
           />
         </div>
 
         <div className="my-55 ml-40">
-          <AiOutlineArrowRight
-            size={50}
-            className="text-asme-blue-600 cursor-pointer text-xl"
-          />
+          <AiOutlineArrowRight size={50} className="cursor-pointer text-xl" />
         </div>
       </div>
     </div>
