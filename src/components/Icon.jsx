710f468ac3a59d4cb7a183ef613733df4b22e636
Tuan Do <trinhphdo@gmail.com>
2023-10-22 18:51:30 -0700

    changed object name

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import objectName from "../../public/navbar-logo.svg";
+import navbarLogo from "../../public/navbar-logo.svg";
 
 const Icon = () => {
   return (
     <div className="flex">
-      <Image src={objectName} alt="Navbar-Logo" />
+      <Image src={navbarLogo} alt="Navbar-Logo" />
       <div
         className="font-extrabold text-white text-4xl -mb-4"
         style={{ fontWeight: 2000, marginTop: "10px", marginLeft: "10px" }}
