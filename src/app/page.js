e74da11ecf5ad88d0c743741327946db6cfc2f60
Ishaan <sendtoishaan@gmail.com>
2023-10-25 16:26:00 -0700

    create button component #3

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Button from "@/components/Button";
 import Landing from "@/components/home/Landing";
 
 const home = () => {
   return (
     <div>
       <Landing />
-      <Button />
     </div>
   );
 };
