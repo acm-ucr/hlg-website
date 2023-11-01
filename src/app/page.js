29081575dfc4757314c56824418876fd1f9a547e
Lanceart <lancellq@gmail.com>
2023-11-01 16:44:56 -0700

    step: bug fixed | css jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
 import Landing from "@/components/home/Landing";
-import CalendarEvent from "@/components/events/Calendar";
-import events from "@/data/events";
 const home = () => {
   return (
     <div>
-      <CalendarEvent events={events} />
       <Landing />
     </div>
   );
