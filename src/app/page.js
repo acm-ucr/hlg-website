3eee5152414e0928d816a229175933beeeccc6f6
theo <theobfernandez@gmail.com>
2023-11-01 17:52:55 -0700

    add home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Landing from "@/components/home/Landing";
+import Who from "@/components/home/Intro";
+import Sponsors from "@/components/home/Sponsors";
 
 const home = () => {
   return (
     <div>
       <Landing />
+      <Who />
+      <div className="py-10" />
+      <Sponsors />
     </div>
   );
 };
