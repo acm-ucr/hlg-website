930e3befccc4b921efaae6302e7f5b17426f51cc
Ricky Wang <rwang187@ucr.edu>
2023-11-07 21:07:43 -0800

    forgot to revert layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Montserrat } from "next/font/google";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import Teammate from "../components/team/Teammate";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -24,7 +23,6 @@ export default function RootLayout({ children }) {
         <Navigation />
         {children}
         <Footer />
-        <Teammate />
       </body>
     </html>
   );
