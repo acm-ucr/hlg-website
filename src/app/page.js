f0a6efe1a42ccaedd0b467514d1202a0e5a111f9
Ishaan <sendtoishaan@gmail.com>
2023-11-09 00:00:40 -0800

    Make Landing Title Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Button from "@/components/Button";
 import Landing from "@/components/home/Landing";
+import LandingTitle from "@/components/home/LandingTitle";
+
 const home = () => {
   return (
     <div>
       <Landing />
+      <LandingTitle />
     </div>
   );
 };
--- src/components/home/LandingTitle.jsx
+++ src/components/home/LandingTitle.jsx
@@ -1,5 +1,25 @@
+import * as React from "react";
+import Icon from "public/HLG-logo.png";
+import Button from "src/components/Button";
+import Image from "next/image";
+
+console.log(Icon);
+
 const LandingTitle = () => {
-  return <div>LandingTitle</div>;
+  return (
+    <div className="h-screen bg-gradient-to-r flex items-center">
+      <Image src={Icon} alt="Icon" className="w-1/12" />;
+      <div className="flex-call">
+        <p className="text-white font-bold font-sans text-3xl items-center mt-20 flex space-y-2">
+          HIGHLANDER GAMING
+        </p>
+        <p className="text-yellow-500 font-sans text-sm items-center mt-25 flex space-y-2">
+          at University of California
+        </p>
+        <Button />
+      </div>
+    </div>
+  );
 };
 
 export default LandingTitle;
