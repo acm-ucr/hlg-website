0fb9c42274482e58b2a3e9ec2fe69eb38799c0de
John Zhou <zhou9865@gmail.com>
2023-11-08 16:06:28 -0800

    did the thing

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,28 +1,12 @@
 import Banner from "@/components/Banner";
-import Staff from "src/components/staff/Staff.jsx";
+
 import React from "react";
 const about = () => {
   return (
     <div>
       <Banner />
+      
 
-      <div className="flex flex-row gap-12">
-        <Staff
-          posname="position name"
-          name="Evan Littlecat"
-          photo="hlg-website/public/awesome-cat-placeholder.png"
-          discordlink="https://www.google.com"
-          emaillink="https://www.google.com"
-        />
-
-        <Staff
-          posname="position name"
-          name="Jon Z"
-          photo="hlg-website/public/awesome-cat-placeholder.png"
-          discordlink="https://www.google.com"
-          emaillink="https://www.google.com"
-        />
-      </div>
     </div>
   );
 };
