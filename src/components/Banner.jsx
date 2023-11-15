1810d27be0c62abd0bc6b52a70c8fb9b50bb3a1a
javi <javi@Javis-MacBook-Pro-3.local>
2023-11-15 15:53:38 -0800

    add Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,33 @@
-const Banner = () => {
-  return <div>Banner</div>;
+import Pattern from "public/banner-pattern.svg";
+import Image from "next/image";
+
+const Banner = ({ YellowText, WhiteText }) => {
+  return (
+    <div className="top-0  ">
+      <div
+        className="absolute top-0 bottom-0 h-3/4 w-3/5 bg-gradient-to-r from-hlg-black to-hlg-blue-300 
+        flex justify-center items-center"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 75%, 0 45%)" }}
+      >
+        <p className="text-hlg-yellow text-5xl pr-2 pb-[150px] ">
+          {YellowText}
+        </p>
+        <p className="text-hlg-white text-5xl pl-2 pb-[150px] ">{WhiteText}</p>
+      </div>
+
+      <div
+        className="bg-hlg-gray w-2/5 h-3/4 absolute right-0 bottom-0 top-0"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 75%)" }}
+      ></div>
+
+      <div
+        className="absolute right-0 top-0 h-3/4 w-2/5 bg-gradient-to-r from-hlg-blue-300 to-hlg-black"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 45%, 0 75%)" }}
+      >
+        <Image src={Pattern} layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
