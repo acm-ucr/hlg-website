ac9e78fbfe6147322a4b79cfe905135d96e4774a
John Zhou <zhou9865@gmail.com>
2023-11-15 18:14:10 -0800

    after getting rid of the exceeding full page problem, the issue of the image being too big remerged --- and this time, w-3/4 seemingly does nothing

--- src/components/team/Teammates.jsx
+++ src/components/team/Teammates.jsx
@@ -5,7 +5,7 @@ import Heading from "src/components/Heading";
 
 const Teammates = () => {
   return (
-    <div>
+    <div className="border-4 border-red-500 sm:border-green-500 md:border-gray-500 lg:border-blue-500">
       <div className="flex justify-center pb-5">
         <div className="text-3xl font-bold">
           <Heading text={"League of Legends"} />
@@ -19,27 +19,24 @@ const Teammates = () => {
         </div>;
         return (
           <div
-            className="pb-5 pt-2 pr-10 pl-10 justify-center items-center"
+            className="flex flex-col pb-5 pt-2 pr-10 pl-10 justify-center items-center"
             key={index}
           >
-            <div className="flex bg-gradient-to-r from-hlg-blue-300 to-hlg-black">
-              <Image
-                src={player.image}
-                alt="champion Image"
-                className="w-1/2 sm:w-1/4"
-              />
+            <div className="border-4 md:w-11/12 border-purple-500 flex flex-col md:flex-row bg-gradient-to-r from-hlg-blue-300 to-hlg-black">
+              <Image src={player.image} alt="champion Image" />
+              {/* smallest, small, large, w-1/2 sm:w-1/2 lg:w-1/4 */}
 
               <div
-                className="pt-4 pb-4 pl-4 pr-4 sm:pl-10 sm:pr-10 bg-gradient-to-r w-1/2 sm:w-3/4"
+                className="border-4 border-blue-500 p-4 sm:pl-10 sm:pr-10 bg-gradient-to-r md:text-start md:w-3/4 text-center"
                 key={index}
               >
                 <div
-                  className={`bg-gradient-to-r ${player.colorInitial} ${player.colorGradient} bg-clip-text text-transparent font-medium md:font-bold text-base md:text-4xl`}
+                  className={`bg-gradient-to-r ${player.colorInitial} ${player.colorGradient} bg-clip-text text-transparent font-bold sm:font-bold text-base md:text-xl lg:text-4xl`}
                 >
                   <p>{player.name}</p>
                 </div>
 
-                <div className="text-hlg-white pt-2 text-xs sm:text-base">
+                <div className="text-hlg-white pt-2 text-xs sm:text-sm xl:text-2xl">
                   <p className="m-0">YEAR: {player.year}</p>
                   <p className="m-0">POSITION: {player.position}</p>
                   <p className="m-0">CHAMPION: {player.champion}</p>
