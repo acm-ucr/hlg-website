ebe34e70d7b70adb00aaf06d31ad2d23b870998d
Nunu115 <trinhphdo@gmail.com>
2023-11-15 16:24:16 -0800

    hlg white and <a> replaced with <Link>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { FaDiscord } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import Icon from "src/components/Icon.jsx";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-l from-hlg-blue-300 to-hlg-black text-white p-4 flex-1 flex justify-between">
+    <div className="bg-gradient-to-l from-hlg-blue-300 to-hlg-black text-hlg-white p-4 flex-1 flex justify-between">
       <div className="flex items-start mt-2">
         <Icon />
       </div>
@@ -15,15 +16,16 @@ const Footer = () => {
           <div className="ml-4">900 University Ave,</div>
           <div className="ml-4">Riverside, CA 92521</div>
         </div>
-        <div className="border-l-4 border-white h-full mx-4"></div>
+        <div className="border-l-4 border-hlg-white h-full mx-4"></div>
         <div className="flex flex-col items-end">
-          <a
-            href={"https://discord.gg/vyq7GRXC"}
+          <Link
+            href="https://discord.com/invite/hlg"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaDiscord className="text-4xl mb-2"></FaDiscord>
-          </a>
+            <FaDiscord className="text-4xl mb-2" />
+          </Link>
+
           <IoIosMail className="text-4xl"></IoIosMail>
         </div>
       </div>
