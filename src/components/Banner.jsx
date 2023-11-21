ff3ec626657f59ce74950db5e64376bbe080c496
javi <javi@Javis-MacBook-Pro-3.local>
2023-11-21 12:30:26 -0800

    Update Responsivness in different screen sizes and change text size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,26 +5,52 @@ const Banner = ({ YellowText, WhiteText }) => {
   return (
     <div className="top-0  ">
       <div
-        className="absolute top-0 bottom-0 h-3/4 w-3/5 bg-gradient-to-r from-hlg-black to-hlg-blue-300 
+        className="absolute top-0 bottom-0 2xl:h-3/4 2xl:w-3/5 xl:h-3/4 xl:w-3/5 lg:h-3/5 lg:w-3/5  md:h-3/5 md:w-3/5 sm:h-2/5 sm:w-3/5
+
+        max-[639px]:w-3/5 max-[639px]:h-1/2  
+        
+        bg-gradient-to-r from-hlg-black to-hlg-blue-300 
+
         flex justify-center left-0"
         style={{ clipPath: "polygon(0 0, 100% 0, 100% 75%, 0 45%)" }}
       >
-        <p className="text-hlg-yellow font-bold md:text-4xl lg:text-5xl left-0 pr-3 pt-[150px] ">
+        <div
+          className="text-hlg-yellow font-bold max-[639px]:text-xl   sm:text-2xl   md:text-3xl   lg:text-4xl   xl:text-[40px] 2xl:text-[50px] 
+                                                  max-[639px]:pt-[120px] sm:pt-[100px] md:pt-[125px] lg:pt-[140px] xl:pt-[175px]  2xl:pt-[200px]
+                                                  max-[639px]:pr-[7px] 
+                                              
+                      left-0 pr-3 "
+        >
           {YellowText}
-        </p>
-        <p className="text-hlg-white font-bold md:text-4xl lg:text-5xl pr-20 left-0  pt-[150px] ">
+        </div>
+
+        <div
+          className="text-hlg-white font-bold max-[639px]:text-xl   sm:text-2xl md:text-3xl   lg:text-4xl   xl:text-[40px] 2xl:text-[50px] 
+                                                 max-[639px]:pt-[120px] sm:pt-[100px] md:pt-[125px] lg:pt-[140px] xl:pt-[175px]  2xl:pt-[200px]
+                                                 max-[639px]:pr-[10px]
+                   
+                   pr-20 left-0  "
+        >
           {WhiteText}
-        </p>
+        </div>
       </div>
 
       <div
-        className="bg-hlg-gray w-2/5 h-3/4 absolute right-0 bottom-0 top-0"
-        style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 75%)" }}
+        className="bg-hlg-gray 2xl:w-2/5 2xl:h-3/4 xl:h-3/4 xl:w-2/5 lg:h-3/5 lg:w-2/5 md:h-3/5 md:w-2/5 sm:h-2/5 sm:w-2/5
+        
+        max-[639px]:w-2/5 max-[639px]:h-1/2 max-[639px]:right-0 max-[639px]:top-0 
+        
+        absolute right-0 bottom-0 top-0"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 95%, 0 75%)" }}
       ></div>
 
       <div
-        className="absolute right-0 top-0 h-3/4 w-2/5 bg-gradient-to-r from-hlg-blue-300 to-hlg-black"
-        style={{ clipPath: "polygon(0 0, 100% 0, 100% 45%, 0 75%)" }}
+        className="absolute right-0 top-0 2xl:h-3/4 2xl:w-2/5 xl:h-3/4 xl:w-2/5 lg:h-3/5 lg:w-2/5 md:h-3/5 md:w-2/5 sm:h-2/5 sm:w-2/5
+
+        max-[630px]:w-2/5 max-[639px]:h-1/2 max-[639px]:right-0 max-[639px]:top-0 
+
+        bg-gradient-to-r from-hlg-blue-300 to-hlg-black"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 60%, 0 75%)" }}
       >
         <Image src={Pattern} layout="fill" objectFit="cover" />
       </div>
