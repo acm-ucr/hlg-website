11da08a5d0a796cfcf0f52346d8def0e9070b6f3
John Zhou <zhou9865@gmail.com>
2023-11-22 13:43:13 -0800

    ignore this

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import Teammates from "src/components/team/Teammates.jsx";
 
 const team = () => {
   return (
     <div>
       <Banner YellowText={"MEET"} WhiteText={"THE BOARD"} />
+      <Teammates />
     </div>
   );
 };
