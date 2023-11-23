fde3a782576d187a2378f22bea23ae99a435cd76
John Zhou <zhou9865@gmail.com>
2023-11-22 16:28:57 -0800

    fixed all spacings on landing page

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -23,7 +23,7 @@ const Who = () => {
         <div className="flex flex-col basis-5/12 items-center justify-center gap-4">
           <div className="md:py-4" />
           <Heading text={"WHO WE ARE"} />
-          <p className="text-left  m-0 p-0 w-11/12 md:max-w-md">
+          <p className="text-center md:text-left mb-8 p-0 w-11/12 md:max-w-md">
             Founded in April 2012, Highlander Gaming is a social club at the
             University of California, Riverside whose aim is to promote and
             involve the campus and surrounding areas in esports and the gaming
@@ -32,7 +32,7 @@ const Who = () => {
         </div>
       </div>
       <div className="md:pt-20 lg:pt-[150px]" />
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-row justify-center mt-8">
         <p className="rounded-3xl bg-hlg-gray w-11/12 md:w-8/12 p-8 text-center">
           Through Highlander Gaming, we hope to create a strong community that
           dedicates itself to a competitive spirit, while maintaining a strong
@@ -41,11 +41,11 @@ const Who = () => {
       </div>
       <div className="md:pb-44" />
 
-      <div className="text-3xl font-bold pb-3">
+      <div className="text-3xl font-bold pb-3 mt-4 mb-4">
         <Heading text={"Join HLG@UCR"} />
       </div>
 
-      <div className="flex flex-row justify-center pt-2 bg-hlg-gray h-30">
+      <div className="flex flex-row justify-center pt-2 bg-hlg-gray h-30 mb-4">
         <p className="rounded-3xl  md:w-5/12 p-7 text-center">
           To become a student member, follow us online and show up to any
           meetings, events, or projects that interest you!
