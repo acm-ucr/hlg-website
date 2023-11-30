818cd9fe5fdf9498d651d5f8825f9d59bc68f31a
John Zhou <zhou9865@gmail.com>
2023-11-29 16:29:40 -0800

    improved spacing

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -4,10 +4,10 @@ import Heading from "src/components/Heading.jsx";
 
 const Who = () => {
   return (
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center border-4 sm:border-red-800 md:border-yellow-500 lg:border-blue-500">
       <div className="flex md:flex-row flex-col-reverse w-full items-center md:items-start justify-center ">
         {/* left side */}
-        <div className="md:relative justify-center flex md:max-w-sm lg:max-w-md md:w-6/12 md:basis-5/12 ">
+        <div className="md:mt-6 md:relative justify-center flex md:max-w-sm lg:max-w-md md:w-6/12 md:basis-5/12 ">
           <Image
             src={photo}
             className="md:absolute w-11/12 md:top-8 md:left-16 rounded-3xl md:drop-shadow-md z-20 md:w-full md:h-8/12"
@@ -23,7 +23,7 @@ const Who = () => {
         <div className="flex flex-col basis-5/12 items-center justify-center gap-4">
           <div className="md:py-4" />
           <Heading text={"WHO WE ARE"} />
-          <p className="text-center md:text-left mb-8 p-0 w-11/12 md:max-w-md">
+          <p className="text-center md:text-left mb-8 md:mb-0 p-0 w-11/12 md:max-w-md">
             Founded in April 2012, Highlander Gaming is a social club at the
             University of California, Riverside whose aim is to promote and
             involve the campus and surrounding areas in esports and the gaming
@@ -31,17 +31,16 @@ const Who = () => {
           </p>
         </div>
       </div>
-      <div className="md:pt-20 lg:pt-[150px]" />
-      <div className="flex flex-row justify-center mt-8">
+      <div className="pt-8 md:pt-16 lg:pt-[150px]" />
+      <div className="flex flex-row justify-center">
         <p className="rounded-3xl bg-hlg-gray w-11/12 md:w-8/12 p-8 text-center">
           Through Highlander Gaming, we hope to create a strong community that
           dedicates itself to a competitive spirit, while maintaining a strong
           sense of sportsmanship and social inclusion
         </p>
       </div>
-      <div className="md:pb-44" />
 
-      <div className="text-3xl font-bold pb-3 mt-4 mb-4">
+      <div className="text-3xl font-bold pb-3 mt-4 md:mt-0 mb-4 md:mb-0">
         <Heading text={"Join HLG@UCR"} />
       </div>
 
