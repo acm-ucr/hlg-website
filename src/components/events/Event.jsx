729f435186e98bdebd53dfafbcbb09abefbaa197
Ricky Wang <rwang187@ucr.edu>
2023-11-29 16:28:35 -0800

    centered test

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -12,15 +12,17 @@ const Event = ({
   color = "",
 }) => {
   return (
-    <div className="flex  rounded-lg overflow-hidden lg:max-w-2xl md:max-w-lg sm:max-w-md ml-10 mr-10">
-      <div className={`${color} p-4 text-hlg-white text-center w-1/4`}>
-        <div className="lg:text-4xl font-bold md:text-2xl sm:text-xl sm:pt-2">
+    <div className="flex rounded-lg overflow-hidden lg:max-w-2xl md:max-w-lg sm:max-w-md ml-10 mr-10">
+      <div
+        className={`${color} p-4 text-hlg-white text-center w-1/4 items-centerx flex flex-col justify-center`}
+      >
+        <div className="lg:text-4xl text-center font-bold md:text-2xl sm:text-xl sm:pt-2 flex justify-center">
           {month}
         </div>
-        <div className="lg:text-4xl font-semibold md:text-2xl sm:text-2xl">
+        <div className="lg:text-4xl font-semibold md:text-2xl sm:text-2xl flex justify-center">
           {day}
         </div>
-        <div className="lg:text-xl font-normal md:text-lg sm:text-md">
+        <div className="lg:text-xl font-normal md:text-lg sm:text-md flex justify-center">
           {time}
         </div>
       </div>
@@ -36,7 +38,7 @@ const Event = ({
             </Link>
           )}
         </div>
-        <p className="w-full focus:outline-none resize-none font-normal">
+        <p className="w-full focus:outline-none resize-none font-normal mb-4">
           {description}
         </p>
       </div>
