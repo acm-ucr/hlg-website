a2f88e7731adadf1ba056983c98a6465ec315429
Lanceart <lancellq@gmail.com>
2023-12-06 16:23:19 -0800

    step: optimize the data

--- src/components/events/Slides.jsx
+++ src/components/events/Slides.jsx
@@ -1,26 +1,10 @@
 "use client";
 import Image from "next/image";
-import eventPicture from "public/registration-line.png";
-import thirdPicture from "public/beach.png";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import Heading from "../Heading";
 import { useState } from "react";
-
+import { images } from "@/data/images";
 const Slides = () => {
-  const images = [
-    {
-      src: eventPicture,
-      width: 500, // replace with the actual width of the image
-      height: 300,
-    },
-    {
-      src: thirdPicture,
-      width: 500, // replace with the actual width of the image
-      height: 300, // replace with the actual height of the image
-    },
-    // ... other images
-  ];
-
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {
