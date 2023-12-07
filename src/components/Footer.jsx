12ac702e9afd0e25c848c0a7b92c59ea0f3cbef6
John Zhou <zhou9865@gmail.com>
2023-12-06 17:26:09 -0800

    made footer look nicer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,19 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="bg-gradient-to-l from-hlg-blue-300 to-hlg-black text-hlg-white p-2 sm:p-4 flex-1 flex justify-between">
-      <div className="flex items-center mt-2">
+      <div className="flex items-center mt-2 sm:mt-11 ">
         <Icon />
       </div>
-      <div className="flex items-center">
-        <div className="flex flex-col items-start pl-1 sm:pl-4">
+      <div className="flex items-center py-2 sm:py-4">
+        <div className="flex flex-col items-end pl-2 pr-1 sm:px-4">
           <div className="mb-1 text-xs sm:text-sm">contact us</div>
           <div className="text-xs sm:text-sm">900 University Ave,</div>
           <div className="text-xs sm:text-sm">Riverside, CA 92521</div>
         </div>
 
-        <div className="border-l-4 border-hlg-white h-full mx-2"></div>
+        <div className="border-l-4 border-hlg-white h-full mx-2 sm:mx-4"></div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col sm:pr-4">
           <Link
             href="https://discord.com/invite/hlg"
             target="_blank"
