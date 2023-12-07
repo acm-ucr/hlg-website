8d17dd71607d0e8bdc8788de7808552bf8d13b80
Raul Razo <raulrazo@ucr-secure-45-10-13-228-95.wnet.ucr.edu>
2023-12-06 17:11:24 -0800

    Fixed landing component and mobile responsiveness

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,9 @@ import * as React from "react";
 const Button = () => {
   return (
     <div className="">
-      <button className=" md:items-center md:text-md text-sm rounded-full bg-gradient-to-r from-hlg-orange to-hlg-yellow md:px-12 md:py-1 text-white md:font-extrabold font-extrabold border-solid">
-        JOIN US
+      <button className=" md:items-center rounded-full bg-gradient-to-r from-hlg-orange to-hlg-yellow px-9 py-1 text-white md:font-extrabold font-bold border-solid">
+        <div className="md:text-md md:inline hidden">JOIN US</div>
+        <div className="text-sm inline md:hidden">JOIN US</div>
       </button>
     </div>
   );
--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -8,13 +8,15 @@ const Landing = () => {
       <div
         className="absolute top-0 bottom-0
       
-        md:h-4/5 md:w-8/12 sm:h-5/6 h-1/2 w-3/5
+        md:h-4/5 md:w-8/12 h-1/2 w-3/5
        
         bg-gradient-to-r from-hlg-black to-hlg-blue-300
+
+        md:flex md:justify-center md:items-center
         "
         style={{ clipPath: "polygon(0 0, 100% 0, 100% 70%, 0 55%)" }}
       >
-        <div className="flex justify-center items-center h-1/2 ">
+        <div className="flex md:justify-start md:items-start justify-center mb-10 items-center h-1/2 md:mt-0 mt-10">
           <LandingTitle className="" />
         </div>
       </div>
