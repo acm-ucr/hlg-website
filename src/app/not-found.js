fa1f3bcbb586b71a802dd391785cce0c7c53d18a
theo <theobfernandez@gmail.com>
2023-12-14 20:36:52 -0800

    fix footer

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const error = () => {
   return (
-    <div className="justify-center flex flex-col items-center">
+    <div className="justify-center flex flex-1 flex-col items-center">
       <p className="text-hlg-orange text-center text-4xl"> Whoops! </p>
 
       <p className="text-hlg-blue-300 text-center text-8xl "> 404 </p>
