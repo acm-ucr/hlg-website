ca12f9a74274d21a0b07e4d042f171e59c2c969c
theo <theobfernandez@gmail.com>
2023-12-29 19:10:52 -0800

    fix teammates

--- src/data/team.js
+++ src/data/team.js
@@ -1,8 +1,27 @@
+import fireJinx from "public/team/varsity/Jinx_Fire.webp";
 import jinx from "public/team/varsity/Jinx.webp";
 import karma from "public/team/varsity/Karma.webp";
 import malphite from "public/team/varsity/Malphite.webp";
 import nocturne from "public/team/varsity/Nocturne.webp";
 import zed from "public/team/varsity/Zed.webp";
+
+import irelia from "public/team/blue/Irelia.webp";
+import aatrox from "public/team/blue/Aatrox.webp";
+import kaisa from "public/team/blue/Kaisa.webp";
+import leesin from "public/team/blue/LeeSin.webp";
+import rakan from "public/team/blue/Rakan.webp";
+import xayah from "public/team/blue/Xayah.webp";
+
+import ekko from "public/team/gold/Ekko.webp";
+import graves from "public/team/gold/Graves.webp";
+import starJinx from "public/team/gold/Jinx.webp";
+import viktor from "public/team/gold/Viktor.webp";
+import yuumi from "public/team/gold/Yuumi.webp";
+
+import leblanc from "public/team/sub/Leblanc.webp";
+import thresh from "public/team/sub/Thresh.webp";
+import yorick from "public/team/sub/Yorick.webp";
+
 export const team = [
   {
     name: 'Brogan "HDAMFN" McCoy',
@@ -19,39 +38,211 @@ export const team = [
     image: nocturne,
     year: "4",
     position: "JG",
-    champion: "Graves",
+    champion: "Haunting Nocturne",
     color: "hlg-blue-100",
     colorInitial: "from-hlg-blue-100",
     colorGradient: "to-hlg-blue-200",
   },
   {
-    name: 'Jake "LDCs" Han',
+    name: 'Jeffrey "JetAvenger" Huang',
     image: zed,
     year: "4",
     position: "Mid",
-    champion: "Gragas",
-    color: "hlg-orange",
-    colorInitial: "from-hlg-orange",
-    colorGradient: "to-hlg-yellow",
-  },
-  {
-    name: 'Andre "GALAMODE" Amante',
-    image: jinx,
-    year: "4",
-    position: "Bot/ADC",
-    champion: "Aphelios",
+    champion: "Galaxy Slayer Zed",
     color: "hlg-pink",
     colorInitial: "from-hlg-pink",
     colorGradient: "to-hlg-purple",
   },
   {
-    name: 'Chrisian "SlyLego" Tran',
-    image: karma,
-    year: "4",
-    position: "Supp",
-    champion: "Rell",
+    name: 'Joseph "Prince" Kim  ',
+    image: jinx,
+    year: "3",
+    position: "Bot",
+    champion: "Arcane Jinx",
+
     color: "hlg-green",
     colorInitial: "from-hlg-green",
     colorGradient: "to-hlg-teal",
+  },
+  {
+    name: 'Ethan "Asleep" Nguyen',
+    image: karma,
+    year: "4",
+    position: "Supp",
+    champion: "Odyssey Karma",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'Francis "Sarang" San Juan ',
+    image: fireJinx,
+    year: "4",
+    position: "Sub",
+    champion: "Firecracker Jinx",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+];
+
+export const blue = [
+  {
+    name: 'Abdullah "YoSoyMilk" Yazgan',
+    image: aatrox,
+    year: "2",
+    position: "Top",
+    champion: "Mecha Aatrox",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'David "Therion" Truong',
+    image: leesin,
+    year: "5",
+    position: "JG",
+    champion: "Storm Dragon Leesin",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+  {
+    name: 'Henry "Hopefully" Tran',
+    image: irelia,
+    year: "4",
+    position: "Mid",
+    champion: "Mythmaker Irelia",
+    color: "hlg-pink",
+    colorInitial: "from-hlg-pink",
+    colorGradient: "to-hlg-purple",
+  },
+  {
+    name: 'Justin "Jsnak" Pak',
+    image: xayah,
+    year: "2",
+    position: "Bot",
+    champion: "Arcane Xayah",
+
+    color: "hlg-green",
+    colorInitial: "from-hlg-green",
+    colorGradient: "to-hlg-teal",
+  },
+  {
+    name: 'Patrick "我爱你" Liang',
+    image: rakan,
+    year: "2",
+    position: "Supp",
+    champion: "Arcane Rakan",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'Kevin "kYoU" Yu',
+    image: kaisa,
+    year: "2",
+    position: "Sub Bot",
+    champion: "IG Kaisa",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+];
+
+export const gold = [
+  {
+    name: 'Max "AfterHours" Chao',
+    image: aatrox,
+    year: "1",
+    position: "Top",
+    champion: "Mecha Aatrox",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'Philip "Less of you" Ling ',
+    image: ekko,
+    year: "2",
+    position: "JG",
+    champion: "True Damage Ekko",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+  {
+    name: 'Tiger "LittleTiger02" Zhang',
+    image: graves,
+    year: "2",
+    position: "Mid",
+    champion: "Mafia Graves",
+    color: "hlg-pink",
+    colorInitial: "from-hlg-pink",
+    colorGradient: "to-hlg-purple",
+  },
+  {
+    name: 'Kris "물총맨"  Che ',
+    image: viktor,
+    year: "2",
+    position: "Bot",
+    champion: "Classic Viktor",
+
+    color: "hlg-green",
+    colorInitial: "from-hlg-green",
+    colorGradient: "to-hlg-teal",
+  },
+  {
+    name: 'Evan "The Lose Cannon" de Chambeau ',
+    image: starJinx,
+    year: "4",
+    position: "Supp",
+    champion: "Star Guardian Jinx",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'Harim "Rimhang" Chang',
+    image: yuumi,
+    year: "1",
+    position: "Sub Supp",
+    champion: "Classic Yuumi",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+];
+
+export const sub = [
+  {
+    name: 'Numair "Get Toasted" Siddiqui',
+    image: yorick,
+    year: "3",
+    position: "Top",
+    champion: "Base Yorick",
+    color: "hlg-orange",
+    colorInitial: "from-hlg-orange",
+    colorGradient: "to-hlg-yellow",
+  },
+  {
+    name: 'Theodore "Dreammeater" Yun',
+    image: leblanc,
+    year: "6",
+    position: "Mid",
+    champion: "Championship Leblanc",
+    color: "hlg-blue-100",
+    colorInitial: "from-hlg-blue-100",
+    colorGradient: "to-hlg-blue-200",
+  },
+  {
+    name: 'Billy "AprilShower" Liu',
+    image: thresh,
+    year: "2",
+    position: "Supp",
+    champion: "Championship Thresh",
+    color: "hlg-pink",
+    colorInitial: "from-hlg-pink",
+    colorGradient: "to-hlg-purple",
   },
 ];
